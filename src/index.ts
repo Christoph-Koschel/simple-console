d0d5271d8ff94656df5bf15712efe0e4e476f232
Sleepy-Reacon <63058519+Sleepy-Reacon@users.noreply.github.com>
2021-02-15 20:46:38 +0100

    V1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,44 @@
 import {ConsoleEvent} from "./event";
 
-export namespace SimpleConsole {
+export class Console {
+    private readonly process: NodeJS.Process;
 
-    export class Console {
-        private readonly process: NodeJS.Process;
+    public constructor(process: NodeJS.Process) {
+        this.process = process;
+    }
 
-        public constructor(process: NodeJS.Process) {
-            this.process = process;
-        }
+    public emit() {
+        let process: NodeJS.Process = this.process;
+        let args: Array<string> = [];
+        for (let i in process.argv) {
+            if (i === "0" || i === "1" || i === "2") {
+                continue;
+            } else {
+                let value: any = process.argv[i];
+                value = (value === "true") ? true : value;
+                value = (value === "false") ? false : value;
 
-        public emit() {
-            let process: NodeJS.Process = this.process;
-            let args: Array<string> = [];
-            for (let i in process.argv) {
-                if (i === "0" || i === "1" || i === "2") {
-                    continue;
-                } else {
-                    let value: any = process.argv[i];
-                    value = (value === "true") ? true : value;
-                    value = (value === "false") ? false : value;
+                let int = parseFloat(value);
+                value = (isNaN(int)) ? value : int;
 
-                    let int = parseFloat(value);
-                    value = (isNaN(int)) ? value : int;
-
-                    try {
-                        let obj = JSON.parse(value);
-                        value = (typeof obj === "object") ? obj : value;
-                    } catch (err) {
-                    }
-
-                    args.push(value);
+                try {
+                    let obj = JSON.parse(value);
+                    value = (typeof obj === "object") ? obj : value;
+                } catch (err) {
                 }
-            }
-            const values: ConsoleEvent.EventValues = {
-                cwd: process.cwd(),
-                args: args
-            }
 
-            ConsoleEvent.emit(process.argv[2], values);
+                args.push(value);
+            }
+        }
+        const values: ConsoleEvent.EventValues = {
+            cwd: process.cwd(),
+            args: args
         }
 
-        public on(keyword: string, callback: ConsoleEvent.EventCallback, options: ConsoleEvent.EventOptions | undefined = undefined) {
-            ConsoleEvent.on(keyword, callback, options);
-        }
+        ConsoleEvent.emit(process.argv[2], values);
+    }
+
+    public on(keyword: string, callback: ConsoleEvent.EventCallback, options: ConsoleEvent.EventOptions | undefined = undefined) {
+        ConsoleEvent.on(keyword, callback, options);
     }
 }
